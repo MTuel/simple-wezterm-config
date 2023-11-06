42fefe4aa4de94bc170b96a359def93de6820089
ItsLolgical <signupthing@itslolgical.com>
2023-11-05 20:23:32 -0600

    Setup hotkeys for controlling panes and tabs.

--- wezterm.lua
+++ wezterm.lua
@@ -10,15 +10,37 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
--- This is where you actually apply your config choices
-
--- For example, changing the color scheme:
 config.color_scheme = 'Catppuccin Mocha'
 
 config.default_prog = { 'C:\\Program Files\\PowerShell\\7\\pwsh.exe' }
 
 config.font = wezterm.font('CaskaydiaCove Nerd Font Mono')
 config.font_size = 10
+
+config.use_fancy_tab_bar = false
+
+-- Set the leader key to a key that doesn't conflict with other keybindings.
+config.leader = { key = 'z', mods = 'CTRL', timeout_milliseconds = 2000 }
+
+config.keys = {
+
+  -- Creating & Closing Panes
+  { key = 's', mods = 'LEADER', action = wezterm.action.SplitHorizontal },
+  { key = 'v', mods = 'LEADER', action = wezterm.action.SplitVertical },
+  { key = 'w', mods = 'LEADER', action = wezterm.action.CloseCurrentPane { confirm = true } },
+
+  -- Navigating Panes
+  { key = 'h', mods = 'LEADER', action = wezterm.action.ActivatePaneDirection 'Left' },
+  { key = 'l', mods = 'LEADER', action = wezterm.action.ActivatePaneDirection 'Right' },
+  { key = 'k', mods = 'LEADER', action = wezterm.action.ActivatePaneDirection 'Up' },
+  { key = 'j', mods = 'LEADER', action = wezterm.action.ActivatePaneDirection 'Down' },
+
+  -- Creating & Closing Tabs
+  { key = 'n', mods = 'LEADER', action = wezterm.action.SpawnTab 'CurrentPaneDomain' },
+  { key = 'q', mods = 'LEADER', action = wezterm.action.CloseCurrentTab { confirm = true } },
+  { key = 't', mods = 'LEADER', action = wezterm.action.ActivateTabRelative(1) },
+  { key = 't', mods = 'LEADER|SHIFT', action = wezterm.action.ActivateTabRelative(-1) },
+}
 
 -- and finally, return the configuration to wezterm
 return config
