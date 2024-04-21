73bf6333bad7727eac5d214341e2e6bf2ca0af3a
ItsLolgical <signupthing@itslolgical.com>
2024-04-21 17:46:46 -0500

    Added some keybinds.

--- wezterm.lua
+++ wezterm.lua
@@ -26,11 +26,46 @@ config.font = wezterm.font_with_fallback { 'CaskaydiaCove Nerd Font Mono' }
 config.font_size = 10
 config.use_fancy_tab_bar = false
 
+config.colors = {
+  tab_bar = {
+    background = '#22262d',
+
+    active_tab = {
+      bg_color = '#282c34',
+      fg_color = '#dcdcdc',
+      intensity = 'Bold'
+    },
+
+    inactive_tab = {
+      bg_color = '#21252B',
+      fg_color = '#727780',
+      intensity = 'Normal'
+    },
+
+    inactive_tab_hover = {
+      bg_color = '#21252B',
+      fg_color = '#dcdcdc',
+      intensity = 'Bold'
+    },
+
+    new_tab = {
+      bg_color = '#282c34',
+      fg_color = '#727780',
+      intensity = 'Normal'
+    },
+
+    new_tab_hover = {
+      bg_color = '#282c34',
+      fg_color = '#dcdcdc',
+      intensity = 'Bold'
+    },
+  }
+}
+
 -- Set the leader key to a key that doesn't conflict with other keybindings.
 config.leader = { key = 'z', mods = 'CTRL', timeout_milliseconds = 2000 }
 
 config.keys = {
-
   -- Creating & Closing Panes
   { key = 's', mods = 'LEADER', action = wezterm.action.SplitVertical },
   { key = 'v', mods = 'LEADER', action = wezterm.action.SplitHorizontal },
@@ -45,8 +80,23 @@ config.keys = {
   -- Creating & Closing Tabs
   { key = 'n', mods = 'LEADER', action = wezterm.action.SpawnTab 'CurrentPaneDomain' },
   { key = 'q', mods = 'LEADER', action = wezterm.action.CloseCurrentTab { confirm = true } },
+
+  -- Navigating Tabs
   { key = 't', mods = 'LEADER', action = wezterm.action.ActivateTabRelative(1) },
   { key = 't', mods = 'LEADER|SHIFT', action = wezterm.action.ActivateTabRelative(-1) },
+
+  { key = 'F1', mods = 'LEADER', action = wezterm.action.ActivateTab(0) },
+  { key = 'F2', mods = 'LEADER', action = wezterm.action.ActivateTab(1) },
+  { key = 'F3', mods = 'LEADER', action = wezterm.action.ActivateTab(2) },
+  { key = 'F4', mods = 'LEADER', action = wezterm.action.ActivateTab(3) },
+  { key = 'F5', mods = 'LEADER', action = wezterm.action.ActivateTab(4) },
+  { key = 'F6', mods = 'LEADER', action = wezterm.action.ActivateTab(5) },
+  { key = 'F7', mods = 'LEADER', action = wezterm.action.ActivateTab(6) },
+  { key = 'F8', mods = 'LEADER', action = wezterm.action.ActivateTab(7) },
+  { key = 'F9', mods = 'LEADER', action = wezterm.action.ActivateTab(8) },
+  { key = 'F10', mods = 'LEADER', action = wezterm.action.ActivateTab(9) },
+  { key = 'F11', mods = 'LEADER', action = wezterm.action.ActivateTab(10) },
+  { key = 'F12', mods = 'LEADER', action = wezterm.action.ActivateTab(11) },
 }
 
 -- and finally, return the configuration to wezterm
